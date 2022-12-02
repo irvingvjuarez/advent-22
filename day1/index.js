8c038c891dcc51de9a3b28fb30a9b7862ef8fcc2
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-01 19:30:22 -0600

    Challenge 1 fully completed

--- day1/index.js
+++ day1/index.js
@@ -1,9 +1,16 @@
-function getPaper = (gift) => {
+function getPaper (gift) {
 	const size = gift.length
 	return "**" + "*".repeat(size)
 }
 
+function getWrapped(gifts) {
+	return gifts.map(gift => {
+		const wrapping = getPaper(gift)
+		return `${wrapping}\n*${gift}*\n${wrapping}`
+	})
+}
+
 function wrapping(gifts) {
-	const wrapped = gifts.map(gift => `${getPaper(gift)}\n*${gift}*\n${getPaper(gift)}`)
+	const wrapped = getWrapped(gifts)
   return wrapped
 }
