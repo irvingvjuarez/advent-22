7e372b91d1a91bcb195613c7027719b231a5f0f8
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-01 19:27:22 -0600

    Function to get the gift wrapped done in the first challenge

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,9 @@
+function getPaper = (gift) => {
+	const size = gift.length
+	return "**" + "*".repeat(size)
+}
+
 function wrapping(gifts) {
-	const wrapped = gifts.map(gift => `**${"*".repeat(gift.length)}\n*${gift}*\n**${"*".repeat(gift.length)}`)
+	const wrapped = gifts.map(gift => `${getPaper(gift)}\n*${gift}*\n${getPaper(gift)}`)
   return wrapped
 }
