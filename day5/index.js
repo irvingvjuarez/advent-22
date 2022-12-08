22693087da17005a94ce3134baaa026bce4a2158
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-07 23:12:41 -0600

    day5 challenge main structure done, but more tests need to be done yet

--- day5/index.js
+++ day5/index.js
@@ -1,13 +1,29 @@
-const possibleOutputs = {}
+let maxNumber = 0
 
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
 	giftsCities = giftsCities.filter(city => city <= maxGifts)
 	const availableCities = giftsCities.length > 0
-
+	const returnPoint = giftsCities.length <= maxCities
 	let result = 0
 
-	if (availableCities) {
-		console.log("Process...")
+	if (returnPoint) {
+		const currentValue = giftsCities.reduce((previous, current) => previous += current, 0)
+		if (currentValue > maxNumber && currentValue <= maxGifts) {
+			maxNumber = currentValue
+		}
+		return maxNumber
+	} else if (availableCities) {
+		giftsCities.forEach((_cityGifts, cityGiftsIndex) => {
+			const currentGiftsCities = [...giftsCities]
+			currentGiftsCities.splice(cityGiftsIndex, 1)
+			result = getMaxGifts(currentGiftsCities, maxGifts, maxCities)
+
+			if (result > maxNumber && result <= maxGifts) {
+				maxNumber = result
+			}
+		})
+
+		return maxNumber
 	} else {
 		return result
 	}
