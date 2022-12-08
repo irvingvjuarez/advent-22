ead5e5328d6b253faf484d5f71120455bcd996d0
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-08 11:36:54 -0600

    day5 algorithm solved removing maxNumber variable

--- day5/index.js
+++ day5/index.js
@@ -1,5 +1,3 @@
-let maxNumber = 0
-
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
 	giftsCities = giftsCities.filter(city => city <= maxGifts)
 	const availableCities = giftsCities.length > 0
@@ -8,12 +6,12 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 
 	if (returnPoint) {
 		const currentValue = giftsCities.reduce((previous, current) => previous += current, 0)
-		if (currentValue > maxNumber && currentValue <= maxGifts) {
-			maxNumber = currentValue
+		if (currentValue > result && currentValue <= maxGifts) {
+			result = currentValue
 		}
 
 		if (giftsCities.length <= 1) {
-			return maxNumber
+			return result
 		}
 	}
 
@@ -21,14 +19,14 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 		giftsCities.forEach((_cityGifts, cityGiftsIndex) => {
 			const currentGiftsCities = [...giftsCities]
 			currentGiftsCities.splice(cityGiftsIndex, 1)
-			result = getMaxGifts(currentGiftsCities, maxGifts, maxCities)
+			const currentResult = getMaxGifts(currentGiftsCities, maxGifts, maxCities)
 
-			if (result > maxNumber && result <= maxGifts) {
-				maxNumber = result
+			if (currentResult > result && currentResult <= maxGifts) {
+				result = currentResult
 			}
 		})
 
-		return maxNumber
+		return result
 	} else {
 		return result
 	}
@@ -38,40 +36,26 @@ console.log(
 	getMaxGifts([50], 15, 1)
 ) // 0
 
-maxNumber = 0
-
 console.log(
 	getMaxGifts([50], 100, 1)
-) // 50
-
-maxNumber = 0;
+) // 50;
 
 console.log(
 	getMaxGifts([12, 3, 11, 5, 7], 20, 3)
 ) // 20
 
-maxNumber = 0
-
 console.log(
 	getMaxGifts([50, 70], 100, 1)
 ) // 70
-
-maxNumber = 0
 
 console.log(
 	getMaxGifts([50, 70, 30], 100, 2)
 ) // 100
 
-maxNumber = 0
-
 console.log(
 	getMaxGifts([50, 70, 30], 100, 3)
 ) // 100
 
-maxNumber = 0
-
 console.log(
 	getMaxGifts([50, 70, 30], 100, 4)
 ) // 100
-
-maxNumber = 0
