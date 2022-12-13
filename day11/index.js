213a12b3f4564019e04a8673a342d5ae539b8959
Irving Juárez <irvingjuarez274@gmail.com>
2022-12-13 15:46:31 -0600

    One test failing in day11 challenge

--- day11/index.js
+++ day11/index.js
@@ -37,15 +37,17 @@ function getCompleted(part, total) {
 
 	let divisionResult = (partTime / totalTime).toFixed(3)
 
-	console.log({ divisionResult })
 
 	if(divisionResult > 0.5 && divisionResult < 1) {
 		const missingValue = 1 - divisionResult;
 
 		const missingValueInteger = missingValue * 10;
 		const divisionResultInteger = divisionResult * 10;
-		const splittedBy = primeNumbers.find(num => missingValueInteger % num === 0 && divisionResultInteger % num === 0)
-		const numberOfSlices = 10 / splittedBy
+
+		const splittedBy = primeNumbers.find(num => missingValueInteger % num === 0 && divisionResultInteger % num === 0) ?? 3
+		// console.log({ splittedBy })
+
+		const numberOfSlices = Math.round(10 / splittedBy)
 
 		totalTime = numberOfSlices
 		partTime = Math.round(divisionResult * numberOfSlices)
@@ -57,26 +59,28 @@ function getCompleted(part, total) {
   return `${partTime}/${totalTime}`
 }
 
-// console.log(
-// 	getCompleted('01:00:00', '03:00:00')
-// ) // '1/3'
-// console.log(
-// 	getCompleted('02:00:00', '04:00:00')
-// ) // '1/2'
-// console.log(
-// 	getCompleted('01:00:00', '01:00:00')
-// ) // '1/1'
-// console.log(
-// 	getCompleted('00:10:00', '01:00:00')
-// ) // '1/6'
-// console.log(
-// 	getCompleted('01:10:10', '03:30:30')
-// ) // '1/3'
-// console.log(
-// 	getCompleted('02:20:20', '03:30:30')
-// ) // 2/3
+console.log(
+	getCompleted('01:00:00', '03:00:00')
+) // '1/3'
+console.log(
+	getCompleted('02:00:00', '04:00:00')
+) // '1/2'
+console.log(
+	getCompleted('01:00:00', '01:00:00')
+) // '1/1'
+console.log(
+	getCompleted('00:10:00', '01:00:00')
+) // '1/6'
+console.log(
+	getCompleted('01:10:10', '03:30:30')
+) // '1/3'
 
 
+console.log("------------")
 console.log(
 	getCompleted('03:30:30', '05:50:50')
 ) // '3/5
+console.log("------------")
+console.log(
+	getCompleted('02:20:20', '03:30:30')
+) // 2/3
