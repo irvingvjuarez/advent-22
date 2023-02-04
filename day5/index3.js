da3a867e568615bd7868fd2036a8dde87d375690
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-04 10:21:14 -0600

    Possible sums in the array gotten

--- day5/index3.js
+++ day5/index3.js
@@ -1,12 +1,52 @@
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
 	if (giftsCities.length > 1) {
-		// Handling normal process
+		const sums = []
+
+		const sortedCities = giftsCities.sort((a, b) => {
+			let res = 0;
+
+			if (a < b) res = -1
+			if (a > b) res = 1
+
+			return res
+		})
+
+		sortedCities.forEach((_item, index) => {
+			const copiedCities = [...sortedCities]
+			const target = copiedCities.splice(index, 1)
+
+			if (maxCities > target.length) {
+				for (let i = 0; i < (maxCities - 1); i++) {
+					const targetSum = target.reduce((prev, current) => prev + current, 0)
+
+					if (targetSum >= maxGifts) {
+						break;
+					} else {
+						target.push(copiedCities[i])
+					}
+				}
+
+				sums.push(target)
+			} else {
+				sums.push(target)
+			}
+		})
+
+		console.log(sums)
 	}
 
 	const gifts = giftsCities[0]
 	return gifts < maxGifts ? gifts : 0
 }
 
+// console.log(
+// 	getMaxGifts([50], 15, 1)
+// ) // 0
+
 console.log(
-	getMaxGifts([50], 15, 1)
-) // 0
+	getMaxGifts([12, 3, 11, 5, 7], 20, 3)
+)
+
+// console.log(
+// 	getMaxGifts([50, 70], 100, 1)
+// )
