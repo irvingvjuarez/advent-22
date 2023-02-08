63787edab9c32af76933f7f37c99f0b387a73755
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-07 22:58:42 -0600

    algorithm 12 done, but not allowed to use very modern js code

--- day12/index.js
+++ day12/index.js
@@ -1,3 +1,31 @@
 function selectSleigh(distance, sleighs) {
-  return ''
+	sleighs = sleighs.map(sled => ({
+		...sled,
+		totalComsuption: sled.consumption * distance
+	})).sort((a, b) => {
+		let res = 0
+		if (a.totalComsuption > b.totalComsuption) res = 1
+		if (a.totalComsuption < b.totalComsuption) res = -1
+		return res
+	}).filter(sled => sled.totalComsuption <= 20)
+
+	const bestOption = sleighs.pop()
+
+	if (bestOption) {
+		return bestOption.name
+	}
+
+  return null
 }
+
+const distance = 30
+const sleighs = [
+  { name: "Madeval", consumption: 0.5 },
+  { name: "Hyuuh", consumption: 1 },
+  { name: "Lolivier", consumption: 0.7 },
+  { name: "Gorusuke", consumption: 0.3 },
+]
+
+console.log(
+	selectSleigh(distance, sleighs) // => "Madeval"
+)
