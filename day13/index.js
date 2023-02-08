09ac351b273ede0d0e34823e2b6717d0dfc1732d
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-08 12:16:53 -0600

    day13 challenge done

--- day13/index.js
+++ day13/index.js
@@ -1,3 +1,29 @@
 function getFilesToBackup(lastBackup, changes) {
-	return []
+	changes = changes
+		.filter(change => change[1] > lastBackup)
+		.map(change => change[0])
+		.sort((a, b) => {
+			let res = 0
+			if (a > b) res = 1
+			if (a < b) res = -1
+			return res
+		})
+
+	const files = Array.from(new Set(changes))
+
+	return files || []
 }
+
+const lastBackup = 1546300800
+
+const changes = [
+  [ 3, 1546301100 ],
+  [ 2, 1546300800 ],
+  [ 1, 1546300800 ],
+  [ 1, 1546300900 ],
+  [ 1, 1546301000 ]
+]
+
+console.log(
+	getFilesToBackup(lastBackup, changes) // => [ 1, 3 ]
+)
