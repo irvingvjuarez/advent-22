409ec84041c25682dbe7ab61a8bd851236498a5e
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-13 12:55:26 -0600

    First stage of the day11 algorithm done

--- day11/index.js
+++ day11/index.js
@@ -1,84 +1,41 @@
 // Pasa todo el tiempo a segundos, lo sumas y divides part/total. El algoritmo seria de reduccion a su minima expresion iterando por un array de numeros primos, hasta que no haya coincidencias en las dos: part y total
 
 function getCompleted(part, total) {
-	const primeNumbers = [2, 3, 5, 7, 9]
-
-	let partTime = part.split(":").map((time, timeIndex) => {
-		time = Number(time)
-		switch(timeIndex) {
-			case 0:
-				// hours
-				time = time * 60 * 60
-			break;
-			case 1:
-				// minutes
-				time *= 60
-			break;
-		}
-
+	const times = [part, total].map(time => {
 		return time
-	}).reduce((previous, current) => previous += current, 0)
+			.split(":")
+			.map((time, index) => {
+				time = Number(time)
+				if (index === 0) return time * 60 * 60
+				if (index === 1) return time * 60
+				return time
+			})
+			.reduce((prev, curr) => curr + prev, 0)
+	})
 
-	let totalTime = total.split(":").map((time, timeIndex) => {
-		time = Number(time)
-		switch(timeIndex) {
-			case 0:
-				// hours
-				time = time * 60 * 60
-			break;
-			case 1:
-				// minutes
-				time *= 60
-			break;
-		}
-
-		return time
-	}).reduce((previous, current) => previous += current, 0)
-
-	let divisionResult = (partTime / totalTime).toFixed(3)
-
-
-	if(divisionResult > 0.5 && divisionResult < 1) {
-		const missingValue = 1 - divisionResult;
-
-		const missingValueInteger = missingValue * 10;
-		const divisionResultInteger = divisionResult * 10;
-
-		let splittedBy = primeNumbers.find(num => missingValueInteger % num === 0 && divisionResultInteger % num === 0)
-		if (!splittedBy) splittedBy = 3
-
-		const numberOfSlices = Math.round(10 / splittedBy)
-
-		totalTime = numberOfSlices
-		partTime = Math.round(divisionResult * numberOfSlices)
-	} else {
-		totalTime = Math.round(1 / divisionResult)
-		partTime = Math.round(divisionResult * totalTime)
-	}
-
-  return `${partTime}/${totalTime}`
+  return `${times[0]}/${times[1]}`
 }
 
 console.log(
-	getCompleted('01:00:00', '03:00:00')
-) // '1/3'
+	getCompleted('01:00:00', '03:00:00') // '1/3'
+)
 console.log(
-	getCompleted('02:00:00', '04:00:00')
-) // '1/2'
+	getCompleted('02:00:00', '04:00:00') // '1/2'
+)
 console.log(
-	getCompleted('01:00:00', '01:00:00')
-) // '1/1'
+	getCompleted('01:00:00', '01:00:00') // '1/1'
+)
 console.log(
-	getCompleted('00:10:00', '01:00:00')
-) // '1/6'
+	getCompleted('00:10:00', '01:00:00') // '1/6'
+)
 console.log(
-	getCompleted('01:10:10', '03:30:30')
-) // '1/3'
+	getCompleted('01:10:10', '03:30:30') // '1/3'
+)
 
 
 console.log(
-	getCompleted('03:30:30', '05:50:50')
-) // '3/5
+	getCompleted('03:30:30', '05:50:50') // '3/5
+)
 console.log(
-	getCompleted('02:20:20', '03:30:30')
-) // 2/3
+	getCompleted('02:20:20', '03:30:30') // 2/3
+)
