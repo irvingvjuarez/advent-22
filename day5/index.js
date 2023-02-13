519b0d0e1380a8e729e5acd70b340199f0066953
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-13 11:09:23 -0600

    bugfix in return statement/condition

--- day5/index.js
+++ day5/index.js
@@ -1,6 +1,6 @@
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
 	const sum = (target, items, shoots, obj = {}) => {
-		if (target <= 0 || shoots <= 0) return 0
+		if (target <= 0 || shoots <= 0 || !items.length) return 0
 		if (target in obj) return obj[target]
 
 		const results = []
@@ -24,40 +24,44 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
   return sum(maxGifts, giftsCities, maxCities)
 }
 
-const giftsCities = [12, 3, 11]
-const maxGifts = 20
-const maxCities = 2
-
 console.log(
-	getMaxGifts(giftsCities, maxGifts, maxCities) // 15 (12 + 3)
+	getMaxGifts([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000) // 115
 )
 
-const giftsCities2 = [12, 3, 11, 5, 7]
-const maxGifts2 = 20
-const maxCities2 = 3
+// const giftsCities = [12, 3, 11]
+// const maxGifts = 20
+// const maxCities = 2
 
-console.log(
-	getMaxGifts(giftsCities2, maxGifts2, maxCities2) // 20 (12 + 3 + 5)
-)
+// console.log(
+// 	getMaxGifts(giftsCities, maxGifts, maxCities) // 15 (12 + 3)
+// )
 
-console.log(
-	getMaxGifts([12, 3, 11, 5, 7], 20, 3) // 20
-)
-console.log(
-	getMaxGifts([50], 15, 1) // 0
-)
-console.log(
-	getMaxGifts([50], 100, 1) // 50
-)
-console.log(
-	getMaxGifts([50, 70], 100, 1) // 70
-)
-console.log(
-	getMaxGifts([50, 70, 30], 100, 2) // 100
-)
-console.log(
-	getMaxGifts([50, 70, 30], 100, 3) // 100
-)
-console.log(
-	getMaxGifts([50, 70, 30], 100, 4) // 100
-)
+// const giftsCities2 = [12, 3, 11, 5, 7]
+// const maxGifts2 = 20
+// const maxCities2 = 3
+
+// console.log(
+// 	getMaxGifts(giftsCities2, maxGifts2, maxCities2) // 20 (12 + 3 + 5)
+// )
+
+// console.log(
+// 	getMaxGifts([12, 3, 11, 5, 7], 20, 3) // 20
+// )
+// console.log(
+// 	getMaxGifts([50], 15, 1) // 0
+// )
+// console.log(
+// 	getMaxGifts([50], 100, 1) // 50
+// )
+// console.log(
+// 	getMaxGifts([50, 70], 100, 1) // 70
+// )
+// console.log(
+// 	getMaxGifts([50, 70, 30], 100, 2) // 100
+// )
+// console.log(
+// 	getMaxGifts([50, 70, 30], 100, 3) // 100
+// )
+// console.log(
+// 	getMaxGifts([50, 70, 30], 100, 4) // 100
+// )
